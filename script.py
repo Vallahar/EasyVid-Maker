305061f4aa005da4319d4a5b7d476593d5daed78
Vallahar <ferwow22@gmail.com>
2020-09-07 23:43:29 +0100

    Ready?

---
+++ script.py
@@ -0,0 +1,11 @@
+import ffmpeg
+
+input_still = ffmpeg.input("sources/image1.jpg")
+input_audio = ffmpeg.input("sources/music.mp3")
+
+(
+    ffmpeg
+    .concat(input_still, input_audio, v=1, a=1)
+    .output("output/video.mp4")
+    .run(overwrite_output=True)
+)
